fa9306ec0c422f23b4a50d554d4a05fcacb74017
John <john.19col@gmail.com>
2021-08-29 13:40:11 -0700

    Create WorkOutItem component

--- src/features/workouts/components/workouts-list/WorkoutsList.tsx
+++ src/features/workouts/components/workouts-list/WorkoutsList.tsx
@@ -1,10 +1,7 @@
-import React, { useMemo } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 
-import { AppRoutes } from '../../../../routes';
-import { Minutes, Workout } from '../../../../types/workout';
-import { workoutUtils } from '../../../../utils/workout-utils';
 import { workouts } from '../../data/workouts';
+import { WorkOutItem } from './WorkListItem';
 
 import styles from './WorkoutsList.module.css';
 
@@ -18,27 +15,6 @@ const WorkoutsList = () => {
         ))}
       </ul>
     </div>
-  );
-};
-
-const WorkOutItem = ({ workout }: { workout: Workout }) => {
-  const totalDuration: Minutes = useMemo(() => workoutUtils.durationOf(workout), [workout]);
-  return (
-    <Link className={styles.item} to={AppRoutes.Builder.Workout(workout.id)}>
-      <div className="card b-dark-10 p-6 bg-light r-base sk-card-wide">
-        <div className="card-caption mb-2">
-          <span className="caption caption-positive">
-            {workout.level.toLowerCase()} - {totalDuration} minutes
-          </span>
-        </div>
-        <div className="card-head">
-          <h2 className="heading-medium">{workout.name}</h2>
-        </div>
-        <div className="card-body mt-3">
-          <p className="text-medium">{workout.description}</p>
-        </div>
-      </div>
-    </Link>
   );
 };
 
